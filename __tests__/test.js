e6452c0e1c2f217da984f3e8fc2e080b2d2ba27a
Sammy Wong <66samwon@gafe.molndal.se>
2021-11-26 11:20:27 +0100

    Update test.js

--- __tests__/test.js
+++ __tests__/test.js
@@ -9,7 +9,7 @@ describe("Testing with Jest", () => {
       
     // Jest also allows a test to run multiple
     // times using different values
-    test.each([[1, 1, 2], [-1, 1, 0], [3, 2, 5]])(
+    test.each([[1, 1, 2], [-1, 1, 0], [3, 2, 5],[4, 4, 8]])(
     'Does %i + %i equals %i', (a, b, expectedResult) => {
       expect(sum(a, b)).toBe(expectedResult);
     });
