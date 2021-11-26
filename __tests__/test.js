a7d359faddb4a14eb5f1c5fee1e58bd65fff83e2
Sammy Wong <66samwon@gafe.molndal.se>
2021-11-26 11:02:35 +0100

    Add test

---
+++ __tests__/test.js
@@ -0,0 +1,16 @@
+const sum = require('./../sum.js');
+
+describe("Testing with Jest", () => {
+    test("addition", () => {
+      const sumtest = 2 + 3;
+      const expectedResult = 5;
+      expect(sumtest).toEqual(expectedResult);
+    });
+      
+    // Jest also allows a test to run multiple
+    // times using different values
+    test.each([[1, 1, 2], [-1, 1, 0], [3, 2, 5]])(
+    'Does %i + %i equals %i', (a, b, expectedResult) => {
+      expect(sum(a, b)).toBe(expectedResult);
+    });
+  });
